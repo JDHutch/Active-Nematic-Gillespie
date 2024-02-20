b1540708b0d13ac70bcbba4c38a28759185525aa
JDHutch <31283352+JDHutch@users.noreply.github.com>
2024-02-20 11:50:41 +0100

    More python

--- plot_PSD_uq_fit.py
+++ plot_PSD_uq_fit.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.colors import Normalize
 import matplotlib.cm as cm
+from scipy.optimize import curve_fit as cf
 
 ## Latex Fonts ##
 plt.rc('text', usetex=True)
@@ -22,28 +23,61 @@ def integrand(om,u,q,du,dq,mu,D,chi,rho,e0,ka,kd,a,b,c):
     mu_u = mu*u
     d_q = D*q
     nu_bar = kd*(ex)/(1+ex)
-    return (4/al1)*ex*(mu_u**2 + 2*mu_u*d_q + d_q**2)*nu_bar/((kd*al1/500)**2+(2*np.pi*om)**2)
+    return (4/al1)*ex*(mu_u**2 + 2*mu_u*d_q + d_q**2)*nu_bar/((kd+ kd/al1)**2+(2*np.pi*om)**2)
+
+
+### Fitting ####
+def integrand_fit(om,u,q,du,dq,A):
+    mu = 500
+    D = 50
+    chi = 1000
+    kd = 0.062
+    a = 8100
+    b = 0.46
+    c = 0
+    e0 = -9
+    
+    
+    pkbt = 530E18*1.381E-23*300
+    ex = np.exp(-e0-0.5*(mu*u**2+2*D*u*q+chi*q**2)/pkbt)-a*np.exp(-0.5*b*(mu*(u-c)**2+2*D*q*u+chi*q**2))
+    
+    ka_ar = kd*ex
+    
+
+    al1 = (1+np.sum(ex*du*dq))
+    mu_u = mu*u
+    d_q = D*q
+    ka_ar /= al1
+
+    nu_bar = kd*ka_ar/(kd+ka_ar)
+    return (4/al1)*ex*(mu_u**2 + 2*mu_u*d_q + d_q**2)*nu_bar/((kd/A)**2+(2*np.pi*om)**2)
+
+def igft(om, A):
+    sig = []
+    u = np.linspace(-0.25,0.25,1000)
+    q = np.linspace(-0.25,0.25,1000)
+    du = (np.amax(u)-np.amin(u))/(u.shape[0]-1)
+    dq = (np.amax(q)-np.amin(q))/(q.shape[0]-1)
+    u, q = np.meshgrid(u, q)
+    for o in om:
+        sig.append(np.sum(integrand_fit(o,u, q, du, dq, A)*du*dq))
+    return np.array(sig)
+
+#################
 
 
 n = 10
 mapper = cm.ScalarMappable(Normalize(0,8100), cm.viridis)
 a_ar = []
-plateau = []
-unbd = []
 al1_ar = []
-maxval = []
-grd = []
+fits = []
+
 
 plt.figure()
  
 for i in range(n):
     fold = os.path.join(os.path.expanduser('~'), 'Documents', 'C', 'Gillespie_C', 'Report1_1', '1d_uq_om_') + str(i)
-    psd = np.loadtxt(fold+"/PSD.dat") # t, stress, orientation
-    
-    pop_distrib = np.loadtxt(fold+"/N_Final.dat")
-    unbd_whr = np.where(pop_distrib[:,0] == -2)[0]
-    unbd.append(np.mean(pop_distrib[unbd_whr,1])/1.224490e-02)
-    
+    psd = np.loadtxt(fold+"/PSD.dat") # t, stress, orientation 
     
     f = open(fold + '/parameters.dat')
 
@@ -77,36 +111,28 @@ for i in range(n):
     plt.errorbar(psd[:,0], psd[:,1], yerr=psd[:,2], markeredgecolor=colour, ecolor=colour,
                   markerfacecolor='none', markersize=8, fmt='v', capsize=5, capthick=1, elinewidth=1)
     #plt.scatter(psd[:,0], 2*np.pi*psd[:,1]*psd[:,0], color=colour, fc='none', s=70, marker='v')
+
+    ft = cf(igft, psd[:,0], psd[:,1])
+    fits.append(ft[0])
+
+
+    plt.plot(psd[:,0], igft(psd[:,0], ft[0]), c=colour, alpha=0.5, lw=3)
     
-    om = np.logspace(-6,np.log10(50),100)
+    
     u = np.linspace(-0.25,0.25,1000)
     q = np.linspace(-0.25,0.25,1000)
-
     u_m, q_m = np.meshgrid(u, q)
     du = (np.amax(u)-np.amin(u))/(u.shape[0]-1)
     dq = (np.amax(q)-np.amin(q))/(q.shape[0]-1)
-    sig = []
-    for o in om:
-        sig.append(np.sum(integrand(o, u_m, q_m, du, dq, mu, D_qu, chi, rho, e0, ka, kd, a, b, c)*du*dq))
-    plt.plot(om, np.array(sig), c=colour, alpha=0.5, lw=3)
-    
-    #grd.append(np.polyfit(np.log10(om[64:]), np.log10(om[64:]*np.array(sig)[64:]), 1)[0])
-    
-    plateau.append(sig[0])
-    
     pkbt = rho*1.381E-23*300
     ex = np.exp(-e0-0.5*(mu*u_m**2+2*D_qu*q_m*u_m+chi*q_m**2)/pkbt)-a*np.exp(-0.5*b*(mu*(u_m-c)**2+2*D_qu*q_m*u_m+chi*q_m**2))
     du = (np.amax(u)-np.amin(u))/(u.shape[0]-1)
     al1 = (1+np.sum(ex*du**2))
     al1_ar.append(al1)
     
-    #maxval.append(np.amax(np.array(sig)*om*2*np.pi))
-    maxval.append(np.amax(2*np.pi*psd[:,1]*psd[:,0]))
-    
-    #plt.plot(u, a*np.exp(-b*(u-c)**2), color=colour, lw=2)
-    
-plt.xlim([1E-5, 1])
-plt.ylim([1,7.5E8])   
+
+# plt.xlim([1E-5, 1])
+# plt.ylim([1,7.5E8])   
     
 cb = plt.colorbar(mapper, label='a')
 cb.set_label(label=r'$a$', size='xx-large')
@@ -116,7 +142,7 @@ plt.xscale('log')
 plt.yscale('log')
 plt.xlabel(r"$f/\textrm{Hz}$", size=24)
 plt.ylabel(r"$|\sigma(f)|^2 / \textrm{Pa}^2\textrm{s}$", size=24)
-plt.ylabel(r"$\alpha^{\prime\prime},\, \omega S(\omega) / 2k_B T (\textrm{Pa}^2)$", size=24)
+#plt.ylabel(r"$\alpha^{\prime\prime},\, \omega S(\omega) / 2k_B T (\textrm{Pa}^2)$", size=24)
 plt.tick_params(which='major', direction='in', width=3, labelsize=20, length=6, pad=5, top=True, right=True)
 plt.tick_params(which='minor', direction='in', width=1.5, length=3, top=True, right=True)
     
